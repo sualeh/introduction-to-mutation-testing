714a47366a6ba7fa40cfbd1fb4229bb2de9afd82
Sualeh Fatehi <sualeh@hotmail.com>
2020-05-02 23:57:21 -0400

    Add license, and update code

--- src/main/java/us/fatehi/palindrome/Palindrome.java
+++ src/main/java/us/fatehi/palindrome/Palindrome.java
@@ -6,17 +6,17 @@ public class Palindrome
 
   public boolean isPalindrome(final String word)
   {
+    // Exit early for trivial cases, such as null,
+    // empty string, and a word with a single letter
     if (word == null || word.length() < 2)
     {
       return true;
     }
-    else
-    {
-      final char firstChar = word.charAt(0);
-      final char lastChar = word.charAt(word.length() - 1);
-      final String center = word.substring(1, word.length() - 1);
-      return (firstChar == lastChar) && isPalindrome(center);
-    }
+
+    final char firstChar = word.charAt(0);
+    final char lastChar = word.charAt(word.length() - 1);
+    final String center = word.substring(1, word.length() - 1);
+    return (firstChar == lastChar) && isPalindrome(center);
   }
 
 }
