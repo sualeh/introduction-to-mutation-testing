3101d709fbe682bcd6b0823592641dee02ed1745
Sualeh Fatehi <sualeh@hotmail.com>
2020-06-17 17:14:09 -0400

    Remove unused imports

--- src/test/java/us/fatehi/palindrome/MorePalindromeTest.java
+++ src/test/java/us/fatehi/palindrome/MorePalindromeTest.java
@@ -1,19 +1,15 @@
 package us.fatehi.palindrome;
 
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class MorePalindromeTest
-{
+public class MorePalindromeTest {
 
   private final Palindrome p = new Palindrome();
 
   @Test
-  public void almostPalindrome()
-  {
+  public void almostPalindrome() {
     assertFalse(p.isPalindrome("streets"));
   }
 
