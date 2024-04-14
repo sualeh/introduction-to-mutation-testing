38a26abde14967b89b233f53a3dfcc5a16c42833
Sualeh Fatehi <sualeh@hotmail.com>
2024-04-13 23:14:57 -0400

    Update tests

--- C#/TestPalindrome/PalindromeTest.cs
+++ C#/TestPalindrome/PalindromeTest.cs
@@ -12,37 +12,37 @@ namespace us.fatehi.palindrome.test
         [Test]
         public void EdgeCaseNull()
         {
-            Assert.IsTrue(_p.IsPalindrome(null));
+            Assert.That(_p.IsPalindrome(null));
         }
 
         [Test]
         public void EdgeCaseEmptyString()
         {
-            Assert.IsTrue(_p.IsPalindrome(""));
+            Assert.That(_p.IsPalindrome(""));
         }
 
         [Test]
         public void HappyPathSingleLetter()
         {
-            Assert.IsTrue(_p.IsPalindrome("a"));
+            Assert.That(_p.IsPalindrome("a"));
         }
 
         [Test]
         public void HappyPathEvenLetters()
         {
-            Assert.IsTrue(_p.IsPalindrome("noon"));
+            Assert.That(_p.IsPalindrome("noon"));
         }
 
         [Test]
         public void HappyPathOddLetters()
         {
-            Assert.IsTrue(_p.IsPalindrome("racecar"));
+            Assert.That(_p.IsPalindrome("racecar"));
         }
 
         [Test]
         public void NegativeCase()
         {
-            Assert.IsFalse(_p.IsPalindrome("burning"));
+            Assert.That(_p.IsPalindrome("burning"), Is.False);
         }
     }
 }
